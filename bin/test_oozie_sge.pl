b0ff62819241bcac80659a84671341895300c68c
Brian O'Connor <briandoconnor@gmail.com>
2015-01-28 18:18:56 -0500

    Adding a tool to detect when Oozie is not submitting jobs

---
+++ bin/test_oozie_sge.pl
@@ -0,0 +1,42 @@
+use strict;
+
+my ($qjobs, $ojobs, $wjobs) = find_running();
+
+my $tries = 10;
+while ($qjobs == 0 && $ojobs > 0 && $wjobs > 0) {
+  print "POTENTIAL PROBLEM\n";
+  if ($tries <= 0) { print "ERROR: Restart Oozie 'sudo /etc/init.d/oozie restart'\n"; last; }
+  sleep 60;
+  ($qjobs, $ojobs, $wjobs) = find_running();
+  $tries--;
+}
+
+sub find_running {
+
+  my $qjobs = `qstat | wc -l`;
+  chomp $qjobs;
+
+  my $ojobs = `oozie jobs | grep RUNNING | wc -l`;
+  chomp $ojobs;
+
+  print "There are $qjobs SGE jobs running\n";
+  print "There are $ojobs Oozie jobs running\n";
+
+  my @accessions = split /\n/, `seqware workflow list | grep Accession | awk '{print \$4}'`;
+
+  print "Checking SeqWare Workflows\n";
+  my $running_count = 0;
+  foreach my $accession (@accessions) {
+    chomp $accession;
+    print "  Workflow accession $accession\n";
+    my $running_workflows = `seqware workflow report --accession $accession | grep running | wc -l`;
+    chomp $running_workflows;
+    print "    Running workflows $running_workflows\n";
+    $running_count += $running_workflows;
+  }
+
+  print "The total running workflows $running_count\n";
+
+  return($qjobs, $ojobs, $running_count);
+
+}
