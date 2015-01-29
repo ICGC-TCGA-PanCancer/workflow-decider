b0f9a71867d6f8cdf3803ba68c6d62e4c64528b4
Brian O'Connor <briandoconnor@gmail.com>
2015-01-28 19:10:43 -0500

    This should be run as root

--- bin/test_oozie_sge.pl
+++ bin/test_oozie_sge.pl
@@ -1,13 +1,15 @@
 use strict;
 
+# run this in the root cron
+
 my ($qjobs, $ojobs, $wjobs) = find_running();
 
 my $tries = 10;
 while ($qjobs == 0 && $ojobs > 0 && $wjobs > 0) {
   print "POTENTIAL PROBLEM\n";
   if ($tries <= 0) {
-    print "ERROR: Restart Oozie 'sudo /etc/init.d/oozie restart'\n";
-    my $result = system("sudo /etc/init.d/oozie restart");
+    print "ERROR: Restart Oozie '/etc/init.d/oozie restart'\n";
+    my $result = system("/etc/init.d/oozie restart");
     if ($result != 0) { print "Problems with the restart\n"; }
     last;
   }
@@ -18,23 +20,23 @@ while ($qjobs == 0 && $ojobs > 0 && $wjobs > 0) {
 
 sub find_running {
 
-  my $qjobs = `qstat | wc -l`;
+  my $qjobs = `sudo su -c 'qstat | wc -l' - seqware`;
   chomp $qjobs;
 
-  my $ojobs = `oozie jobs | grep RUNNING | wc -l`;
+  my $ojobs = `sudo su -c 'oozie jobs | grep RUNNING | wc -l' - seqware`;
   chomp $ojobs;
 
   print "There are $qjobs SGE jobs running\n";
   print "There are $ojobs Oozie jobs running\n";
 
-  my @accessions = split /\n/, `seqware workflow list | grep Accession | awk '{print \$4}'`;
+  my @accessions = split /\n/, `sudo su -c 'seqware workflow list | grep Accession | awk "{print \$4}"' - seqware`;
 
   print "Checking SeqWare Workflows\n";
   my $running_count = 0;
   foreach my $accession (@accessions) {
     chomp $accession;
     print "  Workflow accession $accession\n";
-    my $running_workflows = `seqware workflow report --accession $accession | grep running | wc -l`;
+    my $running_workflows = `sudo su -c 'seqware workflow report --accession $accession | grep running | wc -l' - seqware`;
     chomp $running_workflows;
     print "    Running workflows $running_workflows\n";
     $running_count += $running_workflows;
